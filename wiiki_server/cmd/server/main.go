07e0dcb963759bbfff5e0b40c68d51d305c9bb4f
yun_sunjin <sunjin110@gmail.com>
2022-06-12 19:54:56 +0900

    a

--- wiiki_server/cmd/server/main.go
+++ wiiki_server/cmd/server/main.go
@@ -1,8 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"wiiki_server/infra/graph"
+	"wiiki_server/infra/graph/generated"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+)
+
 func main() {
 	// fmt.Println("hello world")
 
-	// srv := handler.NewDefaultServer()
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", 8080), nil))
 
 }
