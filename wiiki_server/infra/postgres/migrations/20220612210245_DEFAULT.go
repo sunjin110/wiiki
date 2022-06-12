47acdb9e07ae31e0afd79c42ac2926e29d1eeadb
yun_sunjin <sunjin110@gmail.com>
2022-06-12 21:51:41 +0900

    a

--- wiiki_server/infra/postgres/migrations/20220612210245_DEFAULT.go
+++ wiiki_server/infra/postgres/migrations/20220612210245_DEFAULT.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"database/sql"
+	"log"
 )
 
 // Up is executed when this migration is applied
 func Up_20220612210245(txn *sql.Tx) {
-	txn.Exec("create table if exists hello(id integer, name varchar(10));")
+	result, err := txn.Exec("create table if exists hello(id integer, name varchar(10));")
+	if err != nil {
+		log.Println("errir is ", err)
+	}
+	log.Println("result is ", result)
 }
 
 // Down is executed when this migration is rolled back
---
+++ wiiki_server/tools.go
@@ -0,0 +1,9 @@
+//go:build tools
+// +build tools
+
+package tools
+
+import (
+	_ "bitbucket.org/liamstask/goose/cmd/goose"
+	_ "github.com/99designs/gqlgen"
+)
